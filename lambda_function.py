780e94657a7ceafc83b96d0acd0c7b5384340737
Prathamesh321 <80764045+Prathamesh321@users.noreply.github.com>
2022-02-15 13:05:44 +0530

    environment variable added

--- lambda_function.py
+++ lambda_function.py
@@ -9,18 +9,20 @@ import boto3
 import io
 from io import StringIO
 def lambda_handler(event, context):
+    key1=process.env.Access_key
+    key2=process.env.Secret_access_key
     s3_file_key = event['Records'][0]['s3']['object']['key'];
     print(s3_file_key)
     print(event)
     bucket = 'sourceetlbucket';
-    s3 = boto3.client('s3', aws_access_key_id='AKIATVIL4LQYAO6DOHQI',  aws_secret_access_key='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')
+    s3 = boto3.client('s3', aws_access_key_id=key1,  aws_secret_access_key=key2)
     obj = s3.get_object(Bucket=bucket, Key=s3_file_key)
     df1 = pd.read_csv(io.BytesIO(obj['Body'].read()));
 
     service_name = 's3'
     region_name = 'ap-south-1'
-    aws_access_key_id = 'AKIATVIL4LQYAO6DOHQI'
-    aws_secret_access_key = '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'
+    aws_access_key_id = key1
+    aws_secret_access_key = key2
 
     s3_resource = boto3.resource(
         service_name=service_name,
